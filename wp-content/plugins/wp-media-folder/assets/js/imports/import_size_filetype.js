42e557cbd137ba538df17b73083ef58c95ce17ee
dharaa.cmarix <dhara.aparnathi@cmarix.com>
2025-07-15 11:33:31 +0530

    plugin commit

---
+++ wp-content/plugins/wp-media-folder/assets/js/imports/import_size_filetype.js
@@ -0,0 +1,41 @@
+(function ($) {
+    if (typeof ajaxurl === "undefined") {
+        ajaxurl = wpmfimport.vars.ajaxurl;
+    }
+
+    $(document).ready(function () {
+        /**
+         * Import size and filetype
+         * @param page
+         */
+        var wpmfimport_meta_size = function (page) {
+            var button_wmpfImportsize = document.getElementById('wmpfImportsize');
+            var $this = jQuery('#wmpfImportsize');
+            $this.find(".spinner").show().css({"visibility": "visible"});
+            /* Ajax import */
+            jQuery.ajax({
+                type: 'POST',
+                url: ajaxurl,
+                data: {
+                    action: "wpmf_import_size_filetype",
+                    wpmf_current_page: page,
+                    wpmf_nonce: wpmfimport.vars.wpmf_nonce
+                },
+                success: function (res) {
+                    if (res.status) {
+                        if (res.continue) {
+                            button_wmpfImportsize.textContent = 'Synchronizing... (' + res.progress + ')';
+                            wpmfimport_meta_size(parseInt(page) + 1)
+                        } else {
+                            $this.closest("div#wpmf_error").hide();
+                        }
+                    }
+                }
+            });
+        };
+
+        $('#wmpfImportsize').on('click', function () {
+            wpmfimport_meta_size(0);
+        });
+    });
+}(jQuery));
